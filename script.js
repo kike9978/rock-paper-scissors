21c63170b1e8f0226fc5df4945fa37df96b23c89
Kike Mendoza <kike.mendoza9978@gmail.com>
2022-06-02 15:13:14 -0500

    Implement animated UI

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+const btnLance = document.querySelector(".lance");
+const btnSword = document.querySelector(".sword");
+const btnAxe = document.querySelector(".axe");
 
 
 // Arreglo de opciones
@@ -19,13 +21,13 @@ function counterPlay() {
 
 // Juega una ronda
 
-function playRound(computerSelection) {
+function playRound(computerSelection, playerSelection) {
 
     console.log(computerSelection);
     
     let ganadorRonda;
 
-    let playerSelection = prompt("Escoge tu arma: piedra, papel o tijeras");
+    // let playerSelection = prompt("Escoge tu arma: piedra, papel o tijeras");
 
     if (playerSelection === null) {
         if (confirm("¿Quieres salir del juego?")) {
@@ -39,8 +41,6 @@ function playRound(computerSelection) {
     }
 
     playerSelection = playerSelection.toLowerCase();
-
-    
 
     if (playerSelection === "") {
         confirm("Cancelado por no querer jugar");
@@ -73,42 +73,58 @@ function playRound(computerSelection) {
     }
 }
 
+btnAxe.addEventListener("click",playRound(counterPlay(), "tijeras"));
+
+
+
 
 function game() {
     let round = 0;
     let playerScore = 0;
     let pcScore = 0;
-    for (let i = 0; i <= 5; i++) {
-        console.log(`Ronda: ${round}`);
-        console.log(`Puntaje de jugador: ${playerScore}`);
-        console.log(`i vale ${i}`)
-        console.log(`Puntaje de la compu: ${pcScore}`);
+    // for (let i = 0; i <= 5; i++) {
+    //     console.log(`Ronda: ${round}`);
+    //     console.log(`Puntaje de jugador: ${playerScore}`);
+    //     console.log(`i vale ${i}`)
+    //     console.log(`Puntaje de la compu: ${pcScore}`);
         
-        let estadoDeRonda;
-        let computerSelection = counterPlay();
-        estadoDeRonda = playRound(computerSelection);
-        if(estadoDeRonda ==="termino"){
-            break;
-        }
+    //     let estadoDeRonda;
+    //     let computerSelection = counterPlay();
+    //     estadoDeRonda = playRound(computerSelection);
+    //     if(estadoDeRonda ==="termino"){
+    //         break;
+    //     }
             
-        if (estadoDeRonda === "empate") {
-            i--;
-            round ++;
+    //     if (estadoDeRonda === "empate") {
+    //         i--;
+    //         round ++;
             
-        } else 
-        if (estadoDeRonda === "perdedor") {
-            pcScore++;
-            round ++;
+    //     } else 
+    //     if (estadoDeRonda === "perdedor") {
+    //         pcScore++;
+    //         round ++;
 
-        } else {
-            playerScore ++;
-            round ++;
-        }
+    //     } else {
+    //         playerScore ++;
+    //         round ++;
+    //     }
 
-    }
+    // }
 }
 
 
-game();
+// Create button variable
+// Battle bgm (maybe from Valentia or smthing)
+// Welcome the player with fire emblem story
+// Ask user for weapon of choice
+// Get player selection
+//  button.addEventListener("click", getButtonClass(){
+//  
+//})
+// Display winner
+//  
+
+
+// game();
 
 
